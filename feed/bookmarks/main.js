c2ce3987786e918928c3e344256c02f47057e4c2
mahdi8180 <148863250+mahdi8180@users.noreply.github.com>
2024-01-20 11:24:36 +0330

    Delete feed directory
    make some changes

--- feed/bookmarks/main.js
+++
@@ -1,7 +0,0 @@
-// setTimeout(function () {
-//   const preLoader = document.getElementById("pre_loader");
-//   preLoader.style.opacity = "0%";
-//   setTimeout(() => {
-//     preLoader.style.display = "none";
-//   }, 400);
-// }, 500);
